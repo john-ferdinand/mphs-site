044525b1ba8bcbbce3490f8b7fed1e54e9be34dc
jordan <ferdinandantonio75@gmail.com>
2018-04-20 15:15:38 +0800

    committing changes from WebStorm 9.0.1

--- js/home.js
+++ js/home.js
@@ -1,20 +1,27 @@
-
+var homeLink = document.getElementById("home-link");
 var ourSchoolLink = document.getElementById("our-school-link");
 var admissionLink = document.getElementById("admission-link");
 var facilitiesLink = document.getElementById("facilities-link");
 var contactUsLink = document.getElementById("contact-us-link");
 
+var carouselContainer = document.getElementById("c10");
 var ourSchool = document.getElementById("our-school");
 var admission = document.getElementById("admission");
 var facilities = document.getElementById("facilities");
 var contactUs = document.getElementById("contact-us");
 
-var container = document.getElementById("c10");
+var container = document.getElementById("c9");
 
+homeLink.addEventListener("click", loadHome);
 ourSchoolLink.addEventListener("click", loadOurSchool);
 admissionLink.addEventListener("click", loadAdmission);
 facilitiesLink.addEventListener("click", loadFacilities);
 contactUsLink.addEventListener("click", loadContactUs);
+
+function loadHome() {
+	alert("Home");
+	container.innerHTML = carouselContainer.innerHTML;
+}
 
 function loadOurSchool(){
 	alert("Our School");
